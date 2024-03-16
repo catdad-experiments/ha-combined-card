a0dd3dcaf3dee7c8e255e68d607f9152d075140a
Kiril Vatev <vatev.1@gmail.com>
2024-03-15 22:19:07 -0400

    cleanup

--- src/combined-card.ts
+++ src/combined-card.ts
@@ -196,7 +196,7 @@ class CombinedCard extends LitElement implements LovelaceCard {
 // Note: this is what adds the card to the UI card selector
 (window as any).customCards = (window as any).customCards || [];
 (window as any).customCards.push({
-    type: NAME,
-    name: "Combined Card",
-    description: "Combine a stack of cards into a single seamless card",
+  type: NAME,
+  name: "Combined Card",
+  description: "Combine a stack of cards into a single seamless card",
 });
