5351bae4a15ff472073478f1bffdf0c7e808326a
Kiril Vatev <vatev.1@gmail.com>
2024-04-14 00:36:08 -0400

    cleaning up the visual editor registration

--- src/combined-card.ts
+++ src/combined-card.ts
@@ -183,11 +183,3 @@ class CombinedCard extends LitElement implements LovelaceCard {
     };
   }
 }
-
-// Note: this is what adds the card to the UI card selector
-(window as any).customCards = (window as any).customCards || [];
-(window as any).customCards.push({
-  type: NAME,
-  name: "Combined Card",
-  description: "Combine a stack of cards into a single seamless card",
-});
