a3774ebb774ebfc5f98585d15fec6d68337523d8
Kiril Vatev <vatev.1@gmail.com>
2025-10-12 01:21:43 -0400

    clean up logging

--- src/utils.ts
+++ src/utils.ts
@@ -2,14 +2,13 @@ import * as pjson from '../package.json';
 
 export type fn = (...args: any[]) => void;
 
-export const NAME = 'combined-card';
-export const EDITOR_NAME = `${NAME}-editor`;
+const PROJECT = 'catdad utility cards';
 
 const pillStyle = (color: string) => `color: ${color}; font-weight: bold; background: #555; border-radius: 2rem`;
 const pillText = (text: string) => `%c ${text} \x1B[m`;
 
 export const LOG = (first: string, ...args: any[]) => {
-  console.log(`${pillText(`${NAME} v${pjson.version}`)} ${first}`, pillStyle('#bad155'), ...args);
+  console.log(`${pillText(`${PROJECT} v${pjson.version}`)} ${first}`, pillStyle('#bad155'), ...args);
 };
 
 export const LOG_EDITOR = (first: string, ...args: any[]) => {
